d7893a1a9a02879aac64413b3019257c7c3189e0
mahabub <mahabubalom72091@gmail.com>
2024-03-24 15:09:21 +0300

    adding

--- data table/js/custom.js
+++ data table/js/custom.js
@@ -1,8 +1,35 @@
+// $(document).ready(function() {
+
+//     var table = $('#example').DataTable({
+
+//         buttons:['copy', 'csv', 'excel', 'pdf', 'print', 'add', 'remove']
+//     });
+
+//     table.buttons().container()
+//     .appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
+
 $(document).ready(function() {
 
     var table = $('#example').DataTable({
 
-        buttons:['copy', 'csv', 'excel', 'pdf', 'print', 'add', 'remove']
+        buttons: [
+            'copy', 
+            'csv', 
+            'excel', 
+            {
+                extend: 'pdf',
+                customize: function (doc) {
+                    // Change page size and orientation to A3
+                    doc.pageOrientation = 'landscape';
+                    doc.pageSize = 'A3';
+                }
+            }, 
+            'print', 
+            'add', 
+            'remove'
+        ]
     });
 
     table.buttons().container()
