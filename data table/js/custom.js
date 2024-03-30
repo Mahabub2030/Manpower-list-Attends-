e9e3d47e941a0e5e53df2d664e61b9ddd510b58a
mahabub <mahabubalom72091@gmail.com>
2024-03-30 11:26:18 +0300

    adding

--- data table/js/custom.js
+++ data table/js/custom.js
@@ -51,6 +51,42 @@
 
 //  secode final code for manpower
 
+// $(document).ready(function() {
+//     var table = $('#example').DataTable({
+//         buttons: [
+//             'copy', 
+//             'csv', 
+//             'excel', 
+//             {
+//                 extend: 'pdf',
+//                 customize: function (doc) {
+//                     // Change page size and orientation to A3
+//                     doc.pageOrientation = 'landscape';
+//                     doc.pageSize = 'A3';
+//                     doc.defaultStyle.fontSize =14;
+                   
+
+//                     // Apply text-center class to 9th and 10th columns in PDF
+//                     doc.content[1].table.body.forEach(function(row) {
+//                         row[8].alignment = 'center'; // 9th column alignment
+//                         row[9].alignment = 'center'; // 10th column alignment
+//                         // doc.styles.tableBody.border = '1px solid #000'; // Add table borders
+//                     });
+//                 }
+//             }, 
+//             'print', 
+//             'add', 
+//             'remove'
+//         ]
+//     });
+
+//     // Apply text-center class to 9th and 10th columns
+//     table.column(8).nodes().to$().addClass('text-center');
+//     table.column(9).nodes().to$().addClass('text-center');
+
+//     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
 $(document).ready(function() {
     var table = $('#example').DataTable({
         buttons: [
@@ -64,14 +100,17 @@ $(document).ready(function() {
                     doc.pageOrientation = 'landscape';
                     doc.pageSize = 'A3';
                     doc.defaultStyle.fontSize = 11;
-                    // doc.styles.tableBody.border = '1px solid #000'; // Add table borders
 
                     // Apply text-center class to 9th and 10th columns in PDF
                     doc.content[1].table.body.forEach(function(row) {
                         row[8].alignment = 'center'; // 9th column alignment
                         row[9].alignment = 'center'; // 10th column alignment
-                        // doc.styles.tableBody.border = '1px solid #000'; // Add table borders
                     });
+
+                    // Increase font size for header
+                    doc.content[1].table.headerRows = 1; // Make sure header is only shown once
+                    doc.styles.tableHeader.fontSize = 14; // Set font size for header
+                    doc.styles.tableHeader.alignment = 'center'
                 }
             }, 
             'print', 
@@ -86,6 +125,114 @@ $(document).ready(function() {
 
     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
 });
+
+
+
+
+// $(document).ready(function() {
+//     var table = $('#example').DataTable({
+//         buttons: [
+//             'copy', 
+//             'pdf', 
+//             'excel', 
+//             {
+//                 extend: 'pdf',
+//                 customize: function (doc) {
+//                     // Change page size and orientation to A3
+//                     doc.pageOrientation = 'landscape';
+//                     doc.pageSize = 'A3';
+//                     doc.defaultStyle.fontSize = 11;
+
+//                     // Apply text-center class to 9th and 10th columns in PDF
+//                     doc.content[1].table.body.forEach(function(row) {
+//                         row[8].alignment = 'center'; // 9th column alignment
+//                         row[9].alignment = 'center'; // 10th column alignment
+//                     });
+
+//                     // Increase font size for header
+//                     doc.content[1].table.headerRows = 1; // Make sure header is only shown once
+//                     doc.styles.tableHeader.fontSize = 14; // Set font size for header
+//                     doc.styles.tableHeader.alignment = 'center';
+
+//                     // Add page numbers
+//                     var pageCount = doc.internal.getNumberOfPages();
+//                     for (var i = 1; i <= pageCount; i++) {
+//                         doc.setPage(i);
+//                         doc.setFontSize(11);
+//                         doc.text('Page ' + i + ' of ' + pageCount, doc.internal.pageSize.width - 60, 10);
+//                     }
+//                 }
+//             }, 
+//             'print', 
+//             'add', 
+//             'remove'
+//         ]
+//     });
+
+//     // Apply text-center class to 9th and 10th columns
+//     table.column(8).nodes().to$().addClass('text-center');
+//     table.column(9).nodes().to$().addClass('text-center');
+
+//     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//     var table = $('#example').DataTable({
+//         buttons: [
+//             'copy', 
+//             'csv', 
+//             'excel', 
+//             {
+//                 extend: 'pdf',
+//                 customize: function (doc) {
+//                     // Change page size and orientation to A3
+//                     doc.pageOrientation = 'landscape';
+//                     doc.pageSize = 'A3';
+//                     doc.defaultStyle.fontSize = 11;
+
+// //                     // Add table borders
+//                     doc.content[1].table.widths = Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+//                     doc.content[1].table.body.forEach(function(row, i) {
+//                         row.forEach(function(cell, j) {
+//                             if (cell.text !== undefined) {
+//                                 if (i === 0) {
+//                                     cell.fillColor = 'black'; // Header background color
+//                                 }
+//                                 cell.border = [0.3, 0.3, 0.3, 0.3]; // Set border for all cells
+//                             }
+//                         });
+//                     });
+
+//                     // Apply text-center class to 9th and 10th columns in PDF
+//                     doc.content[1].table.body.forEach(function(row) {
+//                         row[8].alignment = 'center'; // 9th column alignment
+//                         row[9].alignment = 'center'; // 10th column alignment
+//                     });
+//                 }
+//             }, 
+//             'print', 
+//             'add', 
+//             'remove'
+//         ]
+//     });
+
+//     // Apply text-center class to 9th and 10th columns in DataTable
+//     table.column(8).nodes().to$().addClass('text-center');
+//     table.column(9).nodes().to$().addClass('text-center');
+
+//     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
+
+
+
+
 
 
 
